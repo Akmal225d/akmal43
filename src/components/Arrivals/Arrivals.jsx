accc948db132a6ee4684a4fe665be6660b2605db
Akmal225d <islombahodirov424@gmail.com>
2024-11-29 17:08:19 +0500

    Add files via upload

---
+++ src/components/Arrivals/Arrivals.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Arrivals.scss'
+const Arrivals = () => {
+  return (
+<>
+
+<section className='section2'>
+    <div className="container">
+        <h1>NEW ARRIVALS</h1>
+    </div>
+
+    <div className="cards">
+        <div className="card">
+       
+        </div>
+    </div>
+</section>
+
+
+</>
+  )
+}
+
+export default Arrivals
